f8c584b8c12abb42afb887fb86d935a910b1ee74
AntonovEvgen <eantonoff86@gmail.com>
2025-07-13 16:06:39 +0300

    Multiplay-2

--- 8kyu/Multiply.js
+++ 8kyu/Multiply.js
@@ -2,11 +2,12 @@
 // Этот код не выполняется правильно. Попробуйте выяснить, почему.
 
 // 💥TESTS
-    // assert.strictEqual(multiply(1,1), 1);
-    // assert.strictEqual(multiply(2,1), 2);
-    // assert.strictEqual(multiply(2,2), 4);
-    // assert.strictEqual(multiply(3,5), 15); 
-    // assert.strictEqual(multiply(5,0), 0);
+    assert.strictEqual(multiply(1,1), 1);
+    assert.strictEqual(multiply(2,1), 2);
+    assert.strictEqual(multiply(2,2), 4);
+    assert.strictEqual(multiply(3,5), 15); 
+    assert.strictEqual(multiply(5,0), 0);
+    
 
 function multiply(a, b){
  return a * b
