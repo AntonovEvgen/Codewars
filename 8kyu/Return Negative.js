6ec8e311d8f33153f4b2e185761fb455bec60d1e
AntonovEvgen <eantonoff86@gmail.com>
2025-07-13 16:36:57 +0300

    return-negative-1

--- 8kyu/Return Negative.js
+++ 8kyu/Return Negative.js
@@ -3,8 +3,25 @@
 // и вы должны сделать его отрицательным. 
 // Но, может быть, число уже отрицательное?
 
-    //💥TEST
-    // makeNegative(1);    // return -1
-    // makeNegative(-5);   // return -5
-    // makeNegative(0);    // return 0
-    // makeNegative(0.12); // return -0.12
+//💥TEST
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+
+// ✅SOLUTION
+function makeNegative(num) {
+    if (num > 0) {
+        return -num
+    } else {
+        return num
+    }
+}
+
+console.log(makeNegative(-6));
+
+// ❗Variant №2❗
+
+// function makeNegative(num) {
+//     return num > 0 ? -num : num;
+// }
