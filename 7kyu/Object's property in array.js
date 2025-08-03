d46ad58644b60a4292df4d8bde7bed8a615ab0ae
AntonovEvgen <eantonoff86@gmail.com>
2025-08-03 21:53:17 +0300

    Error_commit

---
+++ 7kyu/Object's property in array.js
@@ -0,0 +1,20 @@
+// Ваша задача - добавить новое свойство usersAnswer к каждому объекту 
+// в массиве questions. Значение usersAnswer должно 
+// быть установлено в null. 
+// Решение должно работать для массива любой длины.
+
+let questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0
+}, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0
+}];
+
+for (let i = 0; i < questions.length; i++) {
+    questions[i].usersAnswer = null;
+}
+
+console.log(questions);
